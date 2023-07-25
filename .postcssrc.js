f6f3fa055275016009df2aa6b705aaa475ce9c72
youngwoonki <dudwn0621@naver.com>
2023-07-25 20:08:37 +0900

    'Create'

---
+++ .postcssrc.js
@@ -0,0 +1,26 @@
+// ESM
+// CommonJS
+
+// import autoprefixer from 'autoprefixer'
+
+// const autoprefixer = require('autoprefixer')
+
+
+
+// export{
+//   plugins:[
+//     autoprefixer
+//   ]
+// }
+
+// module.exports = {
+//   plugins:[
+//     autoprefixer
+//   ]
+// }
+
+module.exports = {
+  plugins:[
+    require('autoprefixer')
+  ]
+}
